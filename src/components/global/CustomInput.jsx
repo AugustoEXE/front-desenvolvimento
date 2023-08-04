f8a4f091d9ab885eb89426d9db1eaa397b386c78
Augusto Cristofoli <106482872+AugustoEXE@users.noreply.github.com>
2023-08-04 17:33:54 -0300

    começei as modal

--- src/components/global/CustomInput.jsx
+++ src/components/global/CustomInput.jsx
@@ -2,7 +2,7 @@ const CustomInput = ({ includedClasses, type, name, id, placeholder }) => {
 
 
     return (
-        <input type={type} name={name} id={id} placeholder={placeholder} className={`${includedClasses} bg-verdinho-escurinho rounded-full text-creminho text-center p-1`} />
+        <input type={type} name={name} id={id} placeholder={placeholder} className={`${includedClasses} bg-verdinho-escurinho  text-creminho text-center p-1`} />
     );
 };
 
