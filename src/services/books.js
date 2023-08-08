ab3407779e4491e94c5aab3f8f8ffb39e8c96dc1
gabrielmadkesantini <santinigabisan@gmail.com>
2023-08-07 23:30:48 -0300

    não vi dar conflito, confia

--- src/services/books.js
+++ src/services/books.js
@@ -1,10 +1,19 @@
+import axios from "axios";
 
-import { useEffect } from "react"
-import axios from "axios"
+const listBooks = async (data) => {
+  return await axios.get("http://localhost:3000/books", { params: data });
+};
 
+const deleteBook = async (id) => {
+  return await axios.get(`http://localhost:3000/book/del/${id}`);
+};
 
-export async function takeBooks(){
-    useEffect(()=>{
-        document
-    })
-}
+const updateBook = async (id, data) => {
+  return await axios.put(`http://localhost:3000/book/${id}`, { data: data });
+};
+
+const createBook = async (id, data) => {
+  return await axios.put(`http://localhost:3000/book`, { data: data });
+};
+
+export { listBooks, deleteBook, updateBook, createBook };
