8b92d9138e4f5887cf09d8e7ecc8bddc978a91a9
Lagostacio <inaciobortolaslago@gmail.com>
2023-08-11 16:45:24 -0300

    cadastro usuario

--- src/services/users.js
+++ src/services/users.js
@@ -1,9 +1,14 @@
 import axios from "axios";
 
-const VITE_API_URL = import.meta.env.VITE_API_URL;
+const API_URL = import.meta.env.VITE_API_URL;
 
 const getCurrentUserData = async (data) => {
-    return await axios.get(`${VITE_API_URL}/user/login`,  data );
+    return await axios.get(`${API_URL}/user/login`,  data );
 };
 
-export { sendLoginData };
+
+export const createUser = async (data) => {
+    return await axios.post(`${API_URL}/create/user`, data)
+}
+
+
