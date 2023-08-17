3874b63f8335ffe95a27630894c8a00fc359cb30
gabrielmadkesantini <santinigabisan@gmail.com>
2023-08-17 01:37:24 -0300

    image added

--- src/components/global/Book.jsx
+++ src/components/global/Book.jsx
@@ -6,35 +6,38 @@ import booksFromAPI from "../../services/books";
 import { useGet } from "../../hooks/useGet";
 import { Link, useNavigate } from "react-router-dom";
 import { useBook } from "./BookProvider";
+const VITE_API_URL = import.meta.env.VITE_API_URL;
 
 const ListofBook = () => {
-  let listBooks = useGet(() => booksFromAPI.listBooks())
+  let listBooks = useGet(() => booksFromAPI.listBooks());
 
-  const {setSelectedBook} = useBook()
-  const navigate = useNavigate()
+  const { setSelectedBook } = useBook();
+  const navigate = useNavigate();
 
-  function handleNavigate (e)  {
-    setSelectedBook({...e})
-    navigate(`/book/${e.id}`)
-    
+  function handleNavigate(e) {
+    setSelectedBook({ ...e });
+    navigate(`/book/${e.id}`);
   }
-
   return listBooks.map((e) => {
-    
-    // console.log(reader.readAsText(e.cover))
+    console.log(e.cover);
     return (
       <div key={e.id} className="flex flex-col  w-60 h-full">
         <img
           className="w-fit h-max shadow-[4.0px_8.0px_8.0px_rgba(0,0,0,0.38)] my-auto"
-          src={new Uint8Array(e.cover?.data).reduce((str, byte) => str + String.fromCharCode(byte), '')}
+          src={VITE_API_URL + "/" + e.cover}
           loading="lazy"
           alt={""}
         />
         <p className="text-center font-bold text-2xl ">{e.name}</p>
         <p className="italic mt-2">{e.author.name}</p>
-          <button type='button' className="col-start-2 col-span-6 bg-laranjinha rounded-full py-1 font-semibold mt-10" key={e.id} onClick={()=>handleNavigate(e)}>
-            Retirar
-          </button>
+        <button
+          type="button"
+          className="col-start-2 col-span-6 bg-laranjinha rounded-full py-1 font-semibold mt-10"
+          key={e.id}
+          onClick={() => handleNavigate(e)}
+        >
+          Retirar
+        </button>
       </div>
     );
   });
--- src/pages/Book.jsx
+++ src/pages/Book.jsx
@@ -8,38 +8,63 @@ import { useBook } from "../components/global/BookProvider";
 const Book = () => {
   const { selectedBook } = useBook();
   // const formattedDate = new Date(currentBook?.release_date)
-  console.warn(selectedBook);
-
+  console.log(selectedBook.cover);
   return (
     <div className="w-full ">
       <Header />
       <div className=" w-3/4 m-auto  bg-azulzinho-escurinho mt-16 rounded-xl">
-        <div className="flex flex-row flex-wrap" >
+        <div className="flex flex-row flex-wrap">
           <img
-          className="scale-75 rounded shadow-[4.0px_8.0px_8.0px_rgba(0,0,0,0.38)]"
-            src={new Uint8Array(selectedBook.cover?.data).reduce((str, byte) => str + String.fromCharCode(byte),"")}
+            className="scale-75 rounded shadow-[4.0px_8.0px_8.0px_rgba(0,0,0,0.38)]"
+            src={new Uint8Array(selectedBook.cover?.data).reduce(
+              (str, byte) => str + String.fromCharCode(byte),
+              ""
+            )}
             alt=""
-            />
-            <div className="my-auto basis">
-              <p className=""><span className="font-bold text-xl">Título:</span> {selectedBook.name}</p>
-              <p className=""><span className="font-bold text-xl">Autor:</span> {selectedBook.author.name}</p>
-              <p className=""><span className="font-bold text-xl">Editora:</span> {selectedBook.publish_company.name}</p>
-              <p className=""><span className="font-bold text-xl">Genero:</span> {selectedBook.genre.name}</p>
-              <p className=""><span className="font-bold text-xl">Numero de páginas:</span> {selectedBook.pages}</p>
-              <p className=""><span className="font-bold text-xl">Data de publicação:</span> {selectedBook.release_date}</p>
-              <p className=""><span className="font-bold text-xl">Idioma:</span> {selectedBook.language}</p>
-              <p className=""><span className="font-bold text-xl">Sinopse:</span> {selectedBook.description}</p>
-              <button className=" bg-laranjinha rounded-full py-1 w-1/2 mt-10 font-semibold " type='submit'>
-            retirar
-          </button>
-
-            </div>
+          />
+          <div className="my-auto basis">
+            <p className="">
+              <span className="font-bold text-xl">Título:</span>{" "}
+              {selectedBook.name}
+            </p>
+            <p className="">
+              <span className="font-bold text-xl">Autor:</span>{" "}
+              {selectedBook.author.name}
+            </p>
+            <p className="">
+              <span className="font-bold text-xl">Editora:</span>{" "}
+              {selectedBook.publish_company.name}
+            </p>
+            <p className="">
+              <span className="font-bold text-xl">Genero:</span>{" "}
+              {selectedBook.genre.name}
+            </p>
+            <p className="">
+              <span className="font-bold text-xl">Numero de páginas:</span>{" "}
+              {selectedBook.pages}
+            </p>
+            <p className="">
+              <span className="font-bold text-xl">Data de publicação:</span>{" "}
+              {selectedBook.release_date}
+            </p>
+            <p className="">
+              <span className="font-bold text-xl">Idioma:</span>{" "}
+              {selectedBook.language}
+            </p>
+            <p className="">
+              <span className="font-bold text-xl">Sinopse:</span>{" "}
+              {selectedBook.description}
+            </p>
+            <button
+              className=" bg-laranjinha rounded-full py-1 w-1/2 mt-10 font-semibold "
+              type="submit"
+            >
+              retirar
+            </button>
+          </div>
         </div>
-
       </div>
-      <div>
-
-      </div>
+      <div></div>
     </div>
   );
 };
